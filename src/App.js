8e8b0f3a363fc58d944b90bd3fa023ee4ebbd9d4
Stephen Payne <koriasutibun@gmail.com>
2022-02-03 23:53:45 -0500

    Can load CSV that's in repo using d3.csv()

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
-import TestPlot from './TestPlot';
+import logo from "./logo.svg";
+import "./App.css";
+import TestPlot from "./TestPlot";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <TestPlot></TestPlot>
-        </a>
+        <TestPlot dataURL="https://raw.githubusercontent.com/Sutidamus/CSSE386-MH/master/data/coordData.csv"></TestPlot>
       </header>
     </div>
   );
--- src/TestPlot.jsx
+++ src/TestPlot.jsx
@@ -1,16 +1,36 @@
-import React from 'react';
+import React from "react";
 import * as d3 from "d3";
 
 class TestPlot extends React.Component {
-    constructor(props){
-        super(props)
-        this.medi = d3.median([1,2,5,10,59,15901,5812])
-    }
+  constructor(props) {
+    super(props);
+    this.dataURL = this.props.dataURL;
+    this.state = { data: [] };
+    this.medi = d3.median([1, 2, 5, 10, 59, 15901, 5812]);
 
-    render() {
-        return <h1>My Median is {this.medi}</h1>;
-    }
-    
+    this.importData = this.importData.bind(this);
+  }
+
+  componentDidMount() {
+    this.importData(this.dataURL);
+  }
+
+  importData(url) {
+    d3.csv(url).then(data => {
+        console.log(data);
+        this.setState({data: data})
+    });
+  }
+
+  render() {
+    console.log(this.state.data);
+    return (
+      <div>
+        <h1>Coordinate Data</h1>
+        {this.state.data.map(o => <p>{o.State}: {o.Longitude}, {o.Latitude}</p>)}
+      </div>
+    );
+  }
 }
 
 export default TestPlot;
